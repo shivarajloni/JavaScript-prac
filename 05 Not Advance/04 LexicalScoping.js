ba8bb39b44909bc9a60af00ec5f630cd6cffb9c7
shivaraj loni <shivarajbloni@gmail.com>
2020-09-06 17:21:52 +0530

    Lexical Scoping

---
+++ 05 Not Advance/04 LexicalScoping.js
@@ -0,0 +1,10 @@
+function init() {
+    var firstName = "Shivaraj";
+    function sayFirstName() {
+        console.log(firstName);
+    }
+    sayFirstName();
+}
+
+init();
+// console.log(firstName);    // not defined! out of context scope
