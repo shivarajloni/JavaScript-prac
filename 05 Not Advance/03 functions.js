430bcd242d714ab139b16a24309d8d7f0a86a631
shivaraj loni <shivarajbloni@gmail.com>
2020-09-06 08:42:14 +0530

    Self executing anonymous function

---
+++ 05 Not Advance/03 functions.js
@@ -0,0 +1,4 @@
+(function sayHello() {
+    console.log("I say Hello");
+    console.log("I again say Hello");
+})();                          // Self Executing Anonymous Function and functional programming
