7d568a14f46269b0afd2c9c8cb2e9f97088eaa65
shivaraj loni <shivarajbloni@gmail.com>
2020-09-07 17:01:47 +0530

    scope in JS, & diff b/w var & let keyword

---
+++ 06 Advance/01 Scope.js
@@ -0,0 +1,15 @@
+console.log(name);
+
+var name = "Shivaraj";
+
+
+if (true) {
+    let lastName = "Loni";   // "let" is allowed & executes only in that scope! or in that block only 
+    console.log(lastName);
+}
+// console.log(lastName);    // using "var" keyword!, this can be accessed & prints the result, 
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
