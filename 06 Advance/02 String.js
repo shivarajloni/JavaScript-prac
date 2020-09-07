3d685ef73e708aa7ee0b260f355791b036934612
shivaraj loni <shivarajbloni@gmail.com>
2020-09-07 17:11:12 +0530

    strings in JS

---
+++ 06 Advance/02 String.js
@@ -0,0 +1,11 @@
+// var name = "Shivaraj"; 
+
+var name = `Shivaraj`;
+var fullName = `${name} Loni`;
+
+console.log(fullName);
+console.log(fullName.charAt(2));
+console.log(fullName.endsWith("?"));
+console.log(fullName.includes("va"));
+console.log(fullName.toLocaleLowerCase());
+
