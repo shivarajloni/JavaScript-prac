70e75b76b249ad93df45920feb43d283bb475df8
shivaraj loni <shivarajbloni@gmail.com>
2020-09-08 13:42:44 +0530

    Inheritance in JS

--- 06 Advance/06ClasssJS.js
+++ 06 Advance/06ClasssJS.js
@@ -14,6 +14,25 @@ class User {
     getCourseList() {
         return this.#courseList;
     }
+    login() {                     
+        return "You are Logged In";
+    }
+
+    static login() {                     // to make the login a private, then use "static" 
+        return "You are Logged In";
+    }
+}
+                                            // Inhertiance
+class SubAdmin extends User{
+    constructor(name, email) {
+        super(name, email);
+    }
+    getAdminInfo(){
+        return "I am Sub-Admin";
+    }
+    login() {
+        return "Login for Admin only!";
+    }
 }
 
 
@@ -31,11 +50,16 @@ module.exports = User;
 // Private Props getters & setters in JS
                             // "#" Let's you to use public and private, & allows getters and setters! 
 const rock = new User("rock", "rock@rock.com");
-console.log(rock.getInfo());
+// console.log(rock.getInfo());
 rock.enrollCourse("C++ Bootcamp");
-console.log(rock.getCourseList());               // Uses Getters
+// console.log(rock.getCourseList());               // Uses Getters
 // console.log(rock.courseList);                   // Same as above line 
 
 
 
 
+const tom = new SubAdmin("Tom", "Tom@jerry.com");
+console.log(tom.getAdminInfo());
+console.log(tom.login());
+
+console.log(tom.getInfo());
