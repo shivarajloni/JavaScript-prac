506985d5211a655bad08563f29972a05b174dd90
shivaraj loni <shivarajbloni@gmail.com>
2020-09-08 17:05:57 +0530

    Handling web API- api-chuck norris

---
+++ 06 Advance/10 API.js
@@ -0,0 +1,18 @@
+fetch('https://api.chucknorris.io/jokes/random')
+.then(response => {
+   return response.json();
+})
+.then((data) => {
+    // console.log("DATA is: ", data);
+    var joke = data.value;
+    console.log("JOKE is: ", joke);
+})
+.catch();
+
+
+
+// try {
+
+// } catch (error) {
+
+// }
