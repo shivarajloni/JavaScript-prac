ffeb24d80083b48af806962f6f8d169bf5d734d6
shivaraj loni <shivarajbloni@gmail.com>
2020-11-05 01:49:31 +0530

    null bool & scope in js

---
+++ nullandBool.js
@@ -0,0 +1,19 @@
+var  temp = 0
+
+console.log("current temp is:" + temp);
+
+
+// grade system
+ // 10 - great job
+ // 5 - work harder
+ // 2 - poor
+ // 0 - fail
+
+ // boolean - true/false
+
+  let actualMarks = 7
+
+  let myGrade = (actualMarks < 10)
+
+  console.log(myGrade);
+    
---
+++ scope.js
@@ -0,0 +1,31 @@
+// let iAmGlobal = 'someValue'
+
+// if (true) {
+//     let iAmLocal = 'someMoreValue'
+//     console.log(iAmGlobal);
+//     console.log(iAmLocal);
+    
+// }
+
+// console.log(iAmLocal);
+// console.log(iAmGlobal);
+
+
+// Kings problem
+
+let king = 'shiva'
+
+ if(true) {
+     let king = 'sam'
+
+     if(true) {
+        // let king = 'Ram'
+         console.log(king);
+
+     }
+ }
+
+ if (true) {
+     console.log("i am second part " + king);
+
+ }
