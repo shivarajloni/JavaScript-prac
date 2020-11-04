ed99e37c2d902ee1f28cb5c36b146ee2b6b29cee
shivaraj loni <shivarajbloni@gmail.com>
2020-11-05 01:35:06 +0530

    cel-fahre & vice-vers conversion

---
+++ celsius.js
@@ -0,0 +1,23 @@
+var score = 100
+
+score = 130
+
+const bonus = 50
+
+var finalScore = (score + bonus) * 1.8
+
+var complex = (4 + 4) * 5
+
+
+// console.log(finalScore);
+// console.log(complex);
+
+
+
+let tempInFahrenheit = 100
+
+// do calculation
+
+let celsius = (tempInFahrenheit - 32) * 5/9
+
+console.log(celsius);
