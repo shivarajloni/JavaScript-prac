5cca30f1b9fa5273e094875f5b4d2af5abd27a02
shivaraj loni <shivarajbloni@gmail.com>
2020-11-17 21:05:57 +0530

    Method- reverse

---
+++ reverse.js
@@ -0,0 +1,11 @@
+var languages = ["JavaScript", "Python", "C++", "Java", "Lua"];
+console.log("Original Array: " + languages);
+
+// reversing array order
+reversed_arr = languages.reverse();
+
+// reverse() modifies the original array
+console.log("Array after reverse(): " + languages);
+
+// reverse() also returns the reversed array
+console.log("Return Value of reverse(): " + reversed_arr);
