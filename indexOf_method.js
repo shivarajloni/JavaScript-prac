1cde241c1b5aaca6bd297dadfa210b99000fa5a3
shivaraj loni <shivarajbloni@gmail.com>
2020-11-17 20:57:31 +0530

    Method- Map

--- indexOf_method.js
+++ indexOf_method.js
@@ -11,3 +11,10 @@ var strPos = str.indexOf("poop");
 
 
 
+//indexOf getting index of array element, returns -1 if not found
+var colors=["red","green","blue"];
+var pos=colors.indexOf("blue");//2
+
+//indexOf getting index of sub string, returns -1 if not found
+var str = "We got a poop cleanup on isle 4.";
+var strPos = str.indexOf("poop");//9
