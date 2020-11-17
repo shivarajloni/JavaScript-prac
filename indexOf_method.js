46cf28bb2b6e3ee4aefedec5a2df69694b3abea5
shivaraj loni <shivarajbloni@gmail.com>
2020-11-17 20:57:16 +0530

    Method- indexOf

---
+++ indexOf_method.js
@@ -0,0 +1,13 @@
+//indexOf - JS method to get index of array element. 
+// Returns -1 if not found
+
+var colors=["red","green","blue"];
+var pos=colors.indexOf("blue");
+
+//indexOf getting index of sub string, returns -1 if not found
+
+var str = "We got a poop cleanup on isle 4.";
+var strPos = str.indexOf("poop");
+
+
+
