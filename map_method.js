bcf74f0055e930a71d7d4e5d45ca49972e6be5dd
shivaraj loni <shivarajbloni@gmail.com>
2020-11-17 20:58:15 +0530

    Method- Map using arrays

--- map_method.js
+++ map_method.js
@@ -1,6 +1,15 @@
-const sweetArray = [2, 3, 4, 5, 35]
-const sweeterArray = sweetArray.map(sweetItem => {
-    return sweetItem * 2
+// const sweetArray = [2, 3, 4, 5, 35]
+// const sweeterArray = sweetArray.map(sweetItem => {
+//     return sweetItem * 2
+// })
+
+// console.log(sweeterArray)
+
+const name = "Sammy"
+const map = Array.prototype.map
+
+const newName = map.call(name, eachLetter => {
+    return `${eachLetter}a`
 })
 
-console.log(sweeterArray)
+console.log(newName)
