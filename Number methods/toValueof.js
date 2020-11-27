7c74818e4c729fac71b74c232dc9d365b57c1905
Shivaraj Loni <42679657+shivarajloni@users.noreply.github.com>
2020-11-27 23:47:01 +0530

    ValueOf

---
+++ Number methods/toValueof.js
@@ -0,0 +1,4 @@
+var x = 123;
+x.valueOf();            // returns 123 from variable x
+(123).valueOf();        // returns 123 from literal 123
+(100 + 23).valueOf();   // returns 123 from expression 100 + 23
