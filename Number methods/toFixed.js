8d2b9af2a8a7d0351693f82a3a02d068eef961da
Shivaraj Loni <42679657+shivarajloni@users.noreply.github.com>
2020-11-27 23:46:07 +0530

    toFixed

---
+++ Number methods/toFixed.js
@@ -0,0 +1,5 @@
+var x = 9.656;
+x.toFixed(0);           // returns 10
+x.toFixed(2);           // returns 9.66
+x.toFixed(4);           // returns 9.6560
+x.toFixed(6);           // returns 9.656000
